42144c8290443b3773e775107f8b4f86e04b0d21
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-10-28 00:04:30 +0100

    completamos consumo de api y renderizado de productos

--- src/app/productos-detalle/productos-detalle.component.ts
+++ src/app/productos-detalle/productos-detalle.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Producto } from '../Model/producto.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-productos-detalle',
   templateUrl: './productos-detalle.component.html',
   styleUrls: ['./productos-detalle.component.css']
 })
-export class ProductosDetalleComponent {
+export class ProductosDetalleComponent implements OnInit{
+    urlApi : string = 'https://fakestoreapi.com/products';
+    productoDetalle? : Producto
 
+    constructor(
+      private _route : ActivatedRoute
+    ){}
+
+    async fetchDetalle(id: number){
+      try {
+        const res = await fetch(`${this.urlApi}/${id}`)
+        const data : Producto = await res.json()
+        this.productoDetalle = data
+        console.log(data)
+      } catch (error) {
+        console.error('Ha ocurrido un error: ', error)
+      }
+    }
+
+    ngOnInit(): void {
+      this._route.params.subscribe( params => {
+         const idProducto =  params['productoId']
+         this.fetchDetalle(idProducto)
+      })
+    }
 }
--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Producto } from '../Model/producto.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
-export class ProductosComponent {
+export class ProductosComponent implements OnInit{
+  urlApi : string = 'https://fakestoreapi.com/products'
+  productos : Producto[] = [];
 
+    constructor(
+      private _router : Router,
+    ){}
+
+  async fetchProductos( ){
+    try {
+      const res = await fetch(this.urlApi)
+      const data : Producto[] = await res.json()
+      this.productos = data
+    } catch (error) {
+      console.error('Ha ocurrido el siguiente error: ', error)
+    }
+  } 
+
+  detalleProductos( id : number ){
+    this._router.navigate(['productos', id])
+  }
+
+  ngOnInit(): void {
+    this.fetchProductos( )
+  }
 }
