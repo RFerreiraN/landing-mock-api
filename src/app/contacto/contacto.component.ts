67151aee857cc2105a893148f482e9101cd42f73
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-11-05 23:45:25 +0100

    refactor

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
@@ -7,42 +7,24 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
-
   formularioContacto: FormGroup;
-
-  constructor(private form: FormBuilder) {
-    // Formulario Reactivo
-    this.formularioContacto = this.form.group({
-      nombre: ['', [Validators.required, Validators.minLength(5)]],
-      email: ['', [Validators.required, Validators.email]]
+  constructor(private fb: FormBuilder) {
+    this.formularioContacto = this.fb.group({
+      nombre : ['', [Validators.required, Validators.minLength(6)]],
+      email : ['', [Validators.required, Validators.email]]
     })
-
+    }
+  
+  enviar( form : FormGroup ){
+      if (form.invalid) {
+    form.markAllAsTouched();
+    return;
   }
-
-  enviarReactivo(form : FormGroup) {
     console.log(form.value)
     form.reset()
   }
 
-  
-  //Formulario de Plantilla
-
-  public usuario = {
-    nombre: '',
-    email: ''
+  hasError( controlItemForm : string, typeError : string){
+    return this.formularioContacto.get(controlItemForm)?.hasError(typeError) && this.formularioContacto.get(controlItemForm)?.touched
   }
-
-  resetForm(form: any) {
-    form.resetForm()
-  }
-
-  enviar(form: any) {
-    if (form.valid) {
-      console.log('Formulario válido: ', this.usuario)
-    } else {
-      console.log('Formulario inválido')
-    }
-    this.resetForm(form)
-  }
-
 }
