56cc030428693369c1868e95d90c4f75e9aeaa92
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-11-06 00:07:06 +0100

    haremos de nuevo el formualrio

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contacto',
@@ -7,24 +7,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
-  formularioContacto: FormGroup;
-  constructor(private fb: FormBuilder) {
-    this.formularioContacto = this.fb.group({
-      nombre : ['', [Validators.required, Validators.minLength(6)]],
-      email : ['', [Validators.required, Validators.email]]
-    })
-    }
   
-  enviar( form : FormGroup ){
-      if (form.invalid) {
-    form.markAllAsTouched();
-    return;
-  }
-    console.log(form.value)
-    form.reset()
-  }
-
-  hasError( controlItemForm : string, typeError : string){
-    return this.formularioContacto.get(controlItemForm)?.hasError(typeError) && this.formularioContacto.get(controlItemForm)?.touched
-  }
 }
+
