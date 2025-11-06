3adf41aea8eb605597a0fb543e62abf978dad837
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-11-06 23:02:04 +0100

    agregamos campo de contraseña y validamos que cumpla ciertas condiciones

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
-  
+  formularioContacto : FormGroup;
+
+ constructor( private fb : FormBuilder ){
+  this.formularioContacto =this.fb.group({
+    nombre : ['', [Validators.required, Validators.minLength(6)]],
+    email : ['', [Validators.required, Validators.email]],
+    password : ['', [Validators.required, Validators.minLength, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')]]
+  })    
+  };
+
+  enviar( form : FormGroup ){
+    console.log(form.value);
+    form.reset();
+  }
+
 }
 
