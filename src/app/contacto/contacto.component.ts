43de07c0aa82ef976f659c5fa7b11349411f37d1
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-11-10 00:13:46 +0100

    refactor

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
@@ -11,37 +10,37 @@ export class ContactoComponent implements OnInit{
   formularioContacto : FormGroup;
   tipoId : string = 'DNI'
 
- constructor( private fb : FormBuilder ){
-
-  this.formularioContacto =this.fb.group({
-    nombre : ['', [Validators.required, Validators.minLength(6)]],
-    email : ['', [Validators.required, Validators.email]],
-    dni : ['', [Validators.required, Validators.minLength(8), Validators.pattern('^([0-9]{8}[A-Z]|[XYZ][0-9]{7}[A-Z]|[A-Z0-9]{6,9})$')]],
-    tipoDni : [''],
-    password : ['', [Validators.required, Validators.minLength(8), Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$')]]
-  })    
-  };
-
-  ngOnInit(): void {
-    this.formularioContacto.get('tipoDni')?.valueChanges.subscribe(valor => {
-      this.tipoId = valor;
-      console.log(this.tipoId)
+  constructor( private fb : FormBuilder ){
+    this.formularioContacto = this.fb.group({
+      nombre : ['', Validators.required],
+      apellidos : ['', Validators.required],
+      id : ['', Validators.required],
+      tipoId : ['DNI'],
+      email : ['', [Validators.required, Validators.email]],
+      password : ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]]
     })
   }
+  ngOnInit(): void {
+    this.formularioContacto.get('tipoId')?.valueChanges.subscribe( valor => {
+      this.tipoId = valor
+    })
 
- enviar(form: FormGroup) {
-  if (form.valid) {
-    console.log('Datos enviados:', form.value);
-    form.reset();
-  } else {
-    form.markAllAsTouched(); 
-  }
-}
+    if(this.tipoId === 'DNI'){
 
-  hasError( controlInput : string, typeError : string ){
-    return this.formularioContacto.get(controlInput)?.hasError(typeError) 
-      && (this.formularioContacto.get(controlInput)?.touched || this.formularioContacto.get(controlInput)?.dirty )
+    }
+
+    //TODO asignar validaciones a cada uno de los casos de id disponibles (dni,nie,pasaporte)
   }
 
+  hasError( inputName : string , typeError : string){
+    return this.formularioContacto.get(inputName)?.hasError(typeError) 
+    && (this.formularioContacto.get(inputName)?.touched || this.formularioContacto.get(inputName)?.dirty)
+  }
+
+  enviar( form : FormGroup ){
+    console.log(form.value)
+    form.reset()
+    
+  }
 }
 
