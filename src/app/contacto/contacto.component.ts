ec532a3a8b0f50f19f45c93021856d607734005c
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-11-10 23:42:36 +0100

    agregamos pattern para los diferentes tipos de identificaion

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -7,40 +7,72 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent implements OnInit{
-  formularioContacto : FormGroup;
+  formularioRegistro : FormGroup;
   tipoId : string = 'DNI'
 
-  constructor( private fb : FormBuilder ){
-    this.formularioContacto = this.fb.group({
+  constructor( private fb: FormBuilder ){
+    this.formularioRegistro = this.fb.group({
       nombre : ['', Validators.required],
       apellidos : ['', Validators.required],
-      id : ['', Validators.required],
+      identificacion : [''],
       tipoId : ['DNI'],
       email : ['', [Validators.required, Validators.email]],
       password : ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]]
     })
   }
   ngOnInit(): void {
-    this.formularioContacto.get('tipoId')?.valueChanges.subscribe( valor => {
-      this.tipoId = valor
-    })
+    this.formularioRegistro.get('tipoId')?.valueChanges.subscribe(valor => {
+      this.tipoId = valor;
+      this.actualizarValidacionIdentificacion(valor)
+    });
 
-    if(this.tipoId === 'DNI'){
+    this.actualizarValidacionIdentificacion(this.tipoId)
+  }
 
+  actualizarValidacionIdentificacion(tipo: string) {
+    const idControl = this.formularioRegistro.get('identificacion');
+
+    if (!idControl) return;
+
+    // Limpiamos validadores previos
+    idControl.clearValidators();
+
+    // Añadimos validadores según tipo
+    switch (tipo) {
+      case 'DNI':
+        idControl.setValidators([
+          Validators.required,
+          Validators.pattern(/^[0-9]{8}[A-Z]$/),
+        ]);
+        break;
+
+      case 'NIE':
+        idControl.setValidators([
+          Validators.required,
+          Validators.pattern(/^[XYZ][0-9]{7}[A-Z]$/),
+        ]);
+        break;
+
+      case 'Pasaporte':
+        idControl.setValidators([
+          Validators.required,
+          Validators.pattern(/^[A-Z0-9]{6,9}$/),
+        ]);
+        break;
     }
 
-    //TODO asignar validaciones a cada uno de los casos de id disponibles (dni,nie,pasaporte)
+    // Re-evaluamos el control para aplicar los nuevos validadores
+    idControl.updateValueAndValidity();
   }
 
-  hasError( inputName : string , typeError : string){
-    return this.formularioContacto.get(inputName)?.hasError(typeError) 
-    && (this.formularioContacto.get(inputName)?.touched || this.formularioContacto.get(inputName)?.dirty)
-  }
-
-  enviar( form : FormGroup ){
-    console.log(form.value)
+  enviar(form : FormGroup){
+    console.log(form.value);
     form.reset()
-    
+  }
+
+  hasError( controlInput : string, typeError : string){
+    return this.formularioRegistro.get(controlInput)?.hasError(typeError) 
+    && (this.formularioRegistro.get(controlInput)?.touched || this.formularioRegistro.get(controlInput)?.dirty) 
   }
 }
 
