092bc664833fd5fcf70771699a3b0f8b9b0087ef
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-11-14 23:18:59 +0100

    refactor form

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -8,74 +8,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent implements OnInit{
-  formularioContacto : FormGroup;
-  tipoId : string = 'DNI'
-
-  constructor( private fb : FormBuilder ){
-    this.formularioContacto = this.fb.group({
-      nombre : ['', Validators.required],
-      apellidos : ['', Validators.required],
-      tipoId : ['DNI'],
-      id : [''],
-      email : ['', [Validators.required, Validators.email]],
-      password : ['', [Validators.required, Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&._-])[A-Za-z\d@$!%*?&._-]+$/)]]
-    })
-  }
-
   ngOnInit(): void {
-    this.formularioContacto.get('tipoId')?.valueChanges.subscribe( valor => {
-      this.tipoId = valor
-      this.validacionCampos(valor)
-    })
-
-    this.validacionCampos(this.tipoId)
+    
   }
-
-  validacionCampos(tipoId : string){
-    const idControl = this.formularioContacto.get('id');
-
-    if(!idControl) return;
-
-    idControl.clearValidators();
-
-    switch(tipoId){
-      case 'DNI':
-        idControl.setValidators([
-          Validators.required,
-          Validators.pattern(/^[0-9]{8}[A-Z]$/)
-        ]);
-        break;
-      
-        case 'NIE':
-          idControl.setValidators([
-            Validators.required,
-            Validators.pattern(/^[XYZ][0-9]{7}[A-Z]$/)
-        ]);
-        break;
-
-        case 'Pasaporte' :
-          idControl.setValidators([
-            Validators.required,
-            Validators.pattern(/^[A-Z0-9]{6,9}$/)
-        ]);
-        break;
-    }
-
-      idControl.updateValueAndValidity();
-  }
-
-  hasError( controlInput : string, typeError : string ){
-   return(
-      this.formularioContacto.get(controlInput)?.hasError(typeError) 
-      && (this.formularioContacto.get(controlInput)?.touched || this.formularioContacto.get(controlInput)?.dirty)
-   ) 
-  }
-
-  enviar(form : FormGroup){
-    console.log(form.value);
-    form.reset()
-  }
-
   
 }
 
