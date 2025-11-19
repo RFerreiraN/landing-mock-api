ecd4b3f8723b94da8927316015f8ba1b542435a7
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-11-19 23:40:43 +0100

    creamos servicio para el consumo de API

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Producto } from '../Model/producto.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private baseURL = 'https://fakestoreapi.com/products'
+
+  constructor( private _httpClient  : HttpClient){ }
+
+  public getProducts(): Observable<Producto[]>{
+    return this._httpClient.get<Producto[]>(this.baseURL)
+  }
+
+  public getProductById( id : number | string ): Observable<Producto>{
+    return this._httpClient.get<Producto>(`${this.baseURL}/${id}`)
+  }
+
+}
