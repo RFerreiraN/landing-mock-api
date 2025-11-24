5a877042f76efdd75c55a78c776b44fe0a3d9478
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-11-24 23:48:49 +0100

    updateProducts

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Producto } from '../Model/producto.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -20,8 +21,17 @@ export class ApiService {
     return this._httpClient.get<Producto>(`${this.baseURL}/${id}`)
   }
 
+    nuevoProducto : Producto =  {
+        id: 25,
+        title: 'cosas',
+        price:  25, 
+        description: 'skate',
+        image: '',
+    }
+
   public updateProduct(id: number | string ) : Observable<Producto>{
-    return this._httpClient.put<Producto>(`${this.baseURL}/${id}`)
+
+    return this._httpClient.put<Producto>(`${this.baseURL}/${id}`, this.nuevoProducto)
   }
 
   public deleteProducto( id : number | string ) : Observable<Producto>{
